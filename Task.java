315844b42936ff59f84db7cec630c8b161db5a9d
lukene1son <kbp68@txstate.edu>
2025-12-02 21:36:27 -0600

    Add files via upload
    Task class added with fields and some basic methods

---
+++ Task.java
@@ -0,0 +1,33 @@
+package todo;
+
+public class Task {
+    private String title;
+    private String description;
+    private boolean completed;
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+        this.completed = false;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+}
