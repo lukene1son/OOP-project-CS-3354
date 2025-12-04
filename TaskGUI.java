112ceabf08252787efabc0be5334a729abdfe8aa
lukene1son <kbp68@txstate.edu>
2025-12-04 15:24:42 -0600

    Created initial GUI window

---
+++ TaskGUI.java
@@ -0,0 +1,15 @@
+package todo;
+
+import javax.swing.*;
+
+public class TaskGUI extends JFrame {
+
+    public TaskGUI() {
+        setTitle("To-Do List");
+        setSize(400, 300);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        // placeholder GUI window only
+        setVisible(true);
+    }
+}
