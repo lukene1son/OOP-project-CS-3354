6a65d13128e8e61b0f0dbc67750ad2246f4f9148
lukene1son <kbp68@txstate.edu>
2025-12-04 19:39:03 -0600

    Refactor Main class to initialize TaskManager and TaskGUI

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package todo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Starting To-Do List project...");
+        TaskManager m = new TaskManager();
+        new TaskGUI(m);
     }
 }
